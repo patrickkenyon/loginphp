b8662364e1721f17f1a83efe0080cebe94dbc46a
Patrick <patrick.kenyon@gmail.com>
2018-09-20 14:03:18 +0100

    created index, login and success pages with basic html structure

---
+++ account.php
@@ -0,0 +1,13 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Successful | Login</title>
+</head>
+<body>
+    <h1>DO PLEASE HAVE A GOOD LOOK AT ALL OF MY BANK DETAILS AND STEAL MY MONEY</h1>
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,19 +1,2 @@
 <?php
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-    <form>
-        <input type = "text" name = "username"> Username<br/>
-        <input type = "text" name = "password"> Password<br/>
-        <input type = "submit">
-    </form>
-</body>
-</html>
-
-<?php
+var_dump($_POST['']);
