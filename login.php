73dc9d69322adf464de77f103473b0a7f4c52eeb
Patrick <patrick.kenyon@gmail.com>
2018-09-20 13:34:25 +0100

    added basic html login, password and submit

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <form>
+        <input type = "text" name = "username"> Username<br/>
+        <input type = "text" name = "password"> Password<br/>
+        <input type = "submit">
+    </form>
+</body>
+</html>
+
+<?php
